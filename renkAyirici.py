25d175f171f8f5d560f3cde84d2814fcbcc0bca3
Emirhan Aluş <39434120+emranalus@users.noreply.github.com>
2020-06-30 20:19:51 +0300

    Update renkAyirici.py

--- renkAyirici.py
+++ renkAyirici.py
@@ -4,7 +4,7 @@ img = cv2.imread("BGR_Original.jpeg") # Source image
 print(img.shape)
 
 # BGR - Blue Green Red - 0 1 2
-# I defined all color channles a seperate variable
+# I defined all color channels a seperate variable
 blue_channle = img[:,:,0]
 green_channle = img[:,:,1]
 red_channle = img[:,:,2]
@@ -13,9 +13,9 @@ print("# > Color Extraction Started!")
 
 while True:
 
-    cv2.imwrite("BlueShow.jpeg", blue_channle) # I merged Blue channle and made it a unique grayscale image
-    cv2.imwrite("GreenShow.jpeg", green_channle) # I merged Green channle and made it a unique grayscale image
-    cv2.imwrite("RedShow.jpeg", red_channle) # I merged Red channle and made it a unique grayscale image
+    cv2.imwrite("BlueShow.jpeg", blue_channle) # I merged Blue channels and made it a unique grayscale image
+    cv2.imwrite("GreenShow.jpeg", green_channle) # I merged Green channels and made it a unique grayscale image
+    cv2.imwrite("RedShow.jpeg", red_channle) # I merged Red channels and made it a unique grayscale image
 
 
     if cv2.waitKey(2000): # Breaks the while loop after 2 second its complated (Its cooler this way xd)
