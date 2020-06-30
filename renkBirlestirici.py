0b12289cccb32c842fbdbbe87613b83ac38f94a8
Emirhan Aluş <39434120+emranalus@users.noreply.github.com>
2020-06-30 20:21:55 +0300

    Update renkBirlestirici.py

--- renkBirlestirici.py
+++ renkBirlestirici.py
@@ -1,13 +1,13 @@
 import cv2
 
-bgr = cv2.imread("BlueShow.jpeg") # I forgot the Blue one with 3 channle so I use my stupidity to my advantage and used it to reconstruct the 3 channle image :)
+bgr = cv2.imread("BlueShow.jpeg") # I forgot the Blue one with 3 channels so I use my stupidity to my advantage and used it to reconstruct the 3 channels image :)
 
 # Reading grayscale images
 blue = cv2.imread("BlueShow.jpeg", cv2.COLOR_BGR2GRAY)
 green = cv2.imread("GreenShow.jpeg", cv2.COLOR_BGR2GRAY)
 red = cv2.imread("RedShow.jpeg", cv2.COLOR_BGR2GRAY)
 
-# In this part I just insert every single grayscale image into the 3 channle image and from 3 grayscale image I generated a 1 BGR image
+# In this part I just insert every single grayscale image into the 3 channels image and from 3 grayscale image I generated a 1 BGR image
 bgr[:,:,0] = blue
 bgr[:,:,1] = green
 bgr[:,:,2] = red
